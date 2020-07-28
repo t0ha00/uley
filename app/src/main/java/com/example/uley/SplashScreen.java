adbf4f2fda93f0e6da5cae66187d6ebd849a540c
Anton <parkhanton@gmail.com>
2020-07-28 16:52:17 +0500

    Splash screen is done and ready to use

--- app/src/main/java/com/example/uley/SplashScreen.java
+++ app/src/main/java/com/example/uley/SplashScreen.java
@@ -2,16 +2,51 @@ package com.example.uley;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3500;
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.splash_screen_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.botom_splash_screen_animation);
+
+        image = findViewById(R.id.uley_logo_image);
+        logo = findViewById(R.id.uley_logo_text);
+        slogan = findViewById(R.id.logo_descr);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
+
+
 }
